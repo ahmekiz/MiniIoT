d19a43057d44a783742aca03912001641e1ed60f
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-18 06:33:45 +0300

    first React Native commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Mini IoT LED</Text>
+      <Text>Device Status OFF</Text>
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#888',
     alignItems: 'center',
     justifyContent: 'center',
   },
