0f1ffe1f9634b67210382947d48a4d98191ccd6d
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-18 06:42:58 +0300

    first jsx to js convert with variable

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const deviceName = 'Mini IoT LED'
   return (
     <View style={styles.container}>
-      <Text>Mini IoT LED</Text>
+      <Text>{deviceName}</Text>
       <Text>Device Status OFF</Text>
     </View>
   );
