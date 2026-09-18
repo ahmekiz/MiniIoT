44f3ad15e6bdd66418648b186ebf28daf1811db6
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-18 07:18:46 +0300

    styles upgrade

--- App.js
+++ App.js
@@ -6,14 +6,20 @@ export default function App() {
   const [isOn, setIsOn] = useState(false);
   const deviceName = 'Mini IoT LED'
   const text = 'Bu bir IoT Projesidir'
+  const info = isOn ? 'LED is shining' : 'LED is currently off'
+  const statusColor= isOn ? 'green' : 'red'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{text}</Text>
       <Text>{deviceName}</Text>
       <Pressable onPress={() => setIsOn(!isOn)}>
-          <Text>{isOn ? 'Turn Off' : 'Turn On'}</Text>
+          <Text style={{color: statusColor}}>{isOn ? 'Turn On' : 'Turn Off'}</Text>
       </Pressable>
-      <Text>Device Status OFF</Text>
+      <Text>
+        {
+          info
+        }
+        </Text>
     </View>
   );
 }
